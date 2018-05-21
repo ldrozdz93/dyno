bd5a3a28341064a68f92d97b392bec5022c9e11e
ldrozdz93 <lukasz.drozdz93@gmail.com>
2018-05-21 21:57:36 +0200

    added storage_helpers.hpp file

---
+++ include/dyno/detail/storage_helpers.hpp
@@ -0,0 +1,39 @@
+// Copyright Lukasz Drozdz 2017
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+namespace dyno
+{
+
+template< std::size_t, std::size_t > class local_storage;
+template< std::size_t sz1, std::size_t sz2 > class sbo_storage;
+struct remote_storage;
+struct shared_remote_storage;
+struct non_owning_storage;
+
+namespace detail
+{
+template< typename > struct is_a_local_storage_t : std::false_type {};
+template< std::size_t sz1, std::size_t sz2 > struct is_a_local_storage_t<local_storage<sz1, sz2> > : std::true_type {};
+
+template< typename > struct is_a_sbo_storage_t : std::false_type {};
+template< std::size_t sz1, std::size_t sz2 > struct is_a_sbo_storage_t<sbo_storage<sz1, sz2> > : std::true_type {};
+
+template< typename T > inline constexpr auto is_a_local_storage = is_a_local_storage_t<T>{};
+template< typename T > inline constexpr auto is_a_sbo_storage = is_a_sbo_storage_t<T>{};
+template< typename T > inline constexpr auto is_a_remote_storage = std::is_same_v<T, remote_storage>;
+template< typename T > inline constexpr auto is_a_shared_remote_storage = std::is_same_v<T, shared_remote_storage>;
+template< typename T > inline constexpr auto is_a_non_owning_storage = std::is_same_v<T, non_owning_storage>;
+
+template< typename T >
+constexpr void static_assert_storage_is_supported()
+{
+  static_assert(   is_a_local_storage<T>
+                || is_a_sbo_storage<T>
+                || is_a_remote_storage<T>
+                || is_a_shared_remote_storage<T>
+                ,"Trying to create a storage using an unsupported other_storage!");
+}
+} // namespace detail
+
+} // namespace dyno
