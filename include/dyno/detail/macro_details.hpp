8da33d92d63b806db223730bf97e30ebbff7e2ea
ldrozdz93 <lukasz.drozdz93@gmail.com>
2018-05-28 22:22:56 +0200

    refactored amcro detail to separate file

---
+++ include/dyno/detail/macro_details.hpp
@@ -0,0 +1,30 @@
+// Copyright Lukasz Drozdz 2018
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+#ifndef DYNO_DETAIL_MACRO_DETAILS_HPP
+#define DYNO_DETAIL_MACRO_DETAILS_HPP
+
+namespace dyno{ namespace detail{
+class Properties
+{
+  using Bitfield = uint32_t;
+  Bitfield prop;
+
+public:
+  enum Property
+  {
+    defaults = 0,
+    non_copy_constructible = 1
+  };
+
+  constexpr Properties(const Bitfield p_prop) :
+    prop(p_prop)
+  {}
+
+  constexpr bool is_copy_construcible() const { return not( non_copy_constructible & prop ); }
+};
+
+}} // namespace dyno namespace detail
+
+#endif //DYNO_DETAIL_MACRO_DETAILS_HPP
