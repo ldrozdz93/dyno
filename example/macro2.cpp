95870b40847c859b03d5359ff38461db14404600
Łukasz Drożdż <lukasz@lukasz-Xubuntu18.04>
2018-08-27 23:08:44 +0200

    refactored macro2.cpp

--- example/macro2.cpp
+++ example/macro2.cpp
@@ -103,96 +103,87 @@ void onHeapSharedExample()
     // ^ won't compile!
 }
 
+void drawOnCout(const Drawable<dyno::macro::visited>& d)
+{
+    d.draw(std::cout);
+}
+
+void visitedExample()
+{
+    using namespace dyno::macro;
+    Circle circle{};
+    Drawable<on_heap>             drawableOnHeap{ Circle{} };
+    Drawable<on_stack<8>>         drawableOnStack{ Square{} };
+    Drawable<on_heap_shared>      drawableShared{ Circle{} };
+    Drawable<on_stack_or_heap<8>> drawableSbo{ Square{} };
+
+    drawOnCout(circle);
+    drawOnCout(drawableOnHeap);
+    drawOnCout(drawableOnStack);
+    drawOnCout(drawableShared);
+    drawOnCout(drawableSbo);
+    // drawOnCout(Square{}); // will not compile with an rvalue!
+}
+
+struct CountedConstruction
+{
+    CountedConstruction() = default;
+    CountedConstruction(const CountedConstruction&) { ++copiedCount; }
+    CountedConstruction(CountedConstruction&&) { ++movedCount; }
+
+    inline static int copiedCount = 0 ;
+    inline static int movedCount = 0 ;
+    static int reset(){ copiedCount = movedCount = 0; }
+};
+
+struct CountedCircle : Circle, CountedConstruction
+{};
+
+void reasonableConstructionExample()
+{
+    using namespace dyno::macro;
+
+    CountedCircle circle{};
+    Drawable<on_heap> drawable{ circle };
+    assert(1 == CountedCircle::copiedCount);
+
+    Drawable<on_stack<8>> drawable2{ CountedCircle{} };
+    assert(1 == CountedCircle::movedCount);
+
+    CountedConstruction::reset();
+
+    Drawable<on_stack<8>> drawable3{ in_place<CountedCircle> };
+    assert(0 == CountedCircle::copiedCount);
+    assert(0 == CountedCircle::movedCount);
+
+    Drawable<on_stack<8>> drawable4{ in_place<CountedCircle> };
+    Drawable<on_heap> drawable5 = drawable4;
+    Drawable<on_heap> drawable6 = std::move(drawable4);
+    assert(1 == CountedCircle::copiedCount);
+    assert(1 == CountedCircle::movedCount);
+
+    CountedConstruction::reset();
+
+    Drawable<on_heap_shared> drawable7{ in_place<CountedCircle> };
+    Drawable<on_heap_shared> drawable8{ drawable7 };
+    assert(0 == CountedCircle::copiedCount);
+
+    Drawable<on_heap> drawable9{ drawable7 };
+    assert(1 == CountedCircle::copiedCount);
+
+    CountedConstruction::reset();
+
+    Drawable<on_heap> drawable10{ in_place<CountedCircle> };
+    Drawable<on_heap> drawable11{ std::move(drawable10) };
+    assert(0 == CountedCircle::movedCount);
+}
+
 int main()
 {
-    std::vector<Drawable<>> vec{ Square{}, Circle{}, Square{}, Circle{} };
-    for(const auto& obj : vec)
-        obj.draw(std::cout);
-
-    vec[0] = vec.back();
-
-    using namespace dyno::macro;
-    boost::container::static_vector< Drawable<on_stack<4> >, 10> stack_vec(
-         vec.begin(), vec.begin() + std::min(vec.size(), 10ul));
-
-    struct Triangle
-    {
-      void draw(std::ostream& out) const { out << name << ","; }
-      int doSomethingElse(){ return 0; }
-      const char* name { "Triangle" };
-    };
-
-    if(stack_vec.size() < stack_vec.capacity())
-        stack_vec.emplace_back( Triangle{} );
-
-    for(const auto& obj : stack_vec)
-        obj.draw(std::cout);
-
-    struct Sphere
-    {
-      void draw(std::ostream& out) const { out << "Sphere,"; }
-      int points[100]{};
-    };
-//    stack_vec.emplace_back( Sphere{} ); // will not copile!
-
-    std::vector<Drawable<on_stack_or_heap<16>>> sbo_vec(stack_vec.begin(), stack_vec.end());
-    sbo_vec.emplace_back( Sphere{} );
-
-    for(const auto& obj : sbo_vec)
-        obj.draw(std::cout);
-
-    Drawable someHeapDrawable{ Sphere{} };
-    sbo_vec.emplace_back(std::move(someHeapDrawable));
-
-    Drawable<on_heap_shared> shared1{ std::move(vec.back()) };
-    vec.pop_back();
-    auto shared2{ shared1 };
-
-    auto drawOnCout = [](const Drawable<visited>& d) // auto& not used just to prove a point ;)
-    {
-        d.draw(std::cout);
-    };
-    Circle circle{};
-    drawOnCout(circle);
-    drawOnCout(vec[0]);
-    drawOnCout(stack_vec[1]);
-    drawOnCout(sbo_vec[2]);
-    drawOnCout(shared1);
-//    drawOnCout(Square{}); // will not compile with an rvalue
-
-    struct Cuboid
-    {
-        Cuboid(const char* name) : name{name} {}
-        void draw(std::ostream& out) const { out << name << ","; }
-        const char* name;
-    };
-
-    vec.emplace_back( in_place<Cuboid>, "Cuboid the Type Erased!" ).draw(std::cout);
-
-    struct NoncopyableLine
-    {
-        NoncopyableLine(const NoncopyableLine&) = delete;
-        NoncopyableLine(NoncopyableLine&&) = default;
-        void draw(std::ostream& out) const { out << "NoncopyableLine,"; }
-    };
-//    Drawable<on_heap> noncopyableDrawable{ NoncopyableLine{} }; // won't compile!
-    Drawable<on_heap, non_copy_constructible> noncopyableDrawable{ NoncopyableLine{} };
-
-    Drawable<on_heap_shared, non_copy_constructible> noncopyableShared1{ std::move(noncopyableDrawable) };
-    auto noncopyableShared2{ noncopyableShared1 };
-
-    struct NonmovableLine
-    {
-        NonmovableLine() = default;
-        NonmovableLine(const NonmovableLine&) = delete;
-        NonmovableLine(NonmovableLine&&) = delete;
-        void draw(std::ostream& out) const { out << "NonmovableLine,"; }
-    };
-    Drawable<on_heap, non_move_constructible> nonmovableDrawable{ in_place<NonmovableLine> }; // must use in_place<>
-    Drawable<on_heap, non_move_constructible> nonmovableDrawable2{ std::move(nonmovableDrawable) }; // move a pointer - legal
-//    Drawable<on_stack<16>, non_move_constructible> nonmovableDrawable3{ std::move(nonmovableDrawable2) }; // move to stack with a vtable - illegal!
-
     basicExample();
     onStackExample();
     sboExample();
+    onHeapSharedExample();
+    visitedExample();
+    reasonableConstructionExample();
 }
